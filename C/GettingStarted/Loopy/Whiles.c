cf481d506c90fa8637d569f98943cab288a9780f
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-07-09 17:47:31 -0600

    Completed while and do while loops in c

---
+++ C/GettingStarted/Loopy/Whiles.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+  int counter = 11;
+  while (counter > 0) {
+    if (counter == 1) {
+      printf("Only %i more bottle to go yo!\n", counter);
+    } else {
+      printf("Drink %i more bottles of beer yo!\n", counter);
+    }
+    counter--;
+  }
+  counter = 11;
+  printf("Done with while loop, moving on to DO WHILE\n");
+  do {
+    if (counter == 1) {
+      printf("Only %i more bottle to go yo!!!\n", counter);
+    } else {
+      printf("Drink %i more bottles of pepto bismol yo!!!\n", counter);
+    }
+    counter--;
+  } while (counter > 0);
+}
